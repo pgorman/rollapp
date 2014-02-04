52d763cad8a8930feb6aa568c40704e58402edf0
Paul Gorman <gorman.paul@gmail.com>
2014-02-04 15:58:10 -0500

    Simplified code, customized colors, added funny DCC dice.

--- rollapp.js
+++ rollapp.js
@@ -2,66 +2,30 @@
 // Paul Gorman, 2014
 // http://devilghost.com/roll
 
-var dice = {
-    historyLength: 32,
-    history: [],
-    sides: 0,
-    toHtml: function () {
-        var html = '';
-        for (i = 0; i < this.history.length; i++) {
-            switch (i) {
-                case 0:
-                    html = '<span class="result0">' + this.history[0] + '</span> ';
-                    break;
-                case 1:
-                    html = html + '<span class="result1">' + this.history[1] + '</span> ';
-                    break;
-                case 2:
-                    html = html + '<span class="result2">' + this.history[2] + '</span> ';
-                    break;
-                case undefined:
-                    html = html + '';
-                    break;
-                default:
-                    html = html + this.history[i] + ' ';
-            }
-        }
-        document.getElementById('d' + this.sides + 'results').innerHTML = html;
-    },
-    roll: function () {
-        var r = Math.floor(Math.random() * this.sides + 1);
-        this.history.unshift(r);
-        if (this.history.length > this.historyLength) {
-            this.history.pop();
-        }
-        this.toHtml(this.history);
+var divs = document.getElementsByTagName('div');
+for (var i = 0; i < divs.length; i++) {
+    if ('ontouchstart' in document.documentElement) {
+        divs[i].ontouchstart = roll;
     }
-};
+    else {
+        divs[i].onclick = roll;
+    }
+}
 
-var d4 = Object.create(dice);
-d4.history = [];
-d4.sides = 4;
+function roll() {
+    var sides = this.getAttribute('data-sides');
+    var span = document.createElement('span');
+    span.innerHTML = Math.floor(Math.random() * sides + 1);
+    this.insertBefore(span, this.firstChild.nextSibling);
+}
 
-var d6 = Object.create(dice);
-d6.history = [];
-d6.sides = 6;
-
-var d8 = Object.create(dice);
-d8.history = [];
-d8.sides = 8;
-
-var d10 = Object.create(dice);
-d10.history = [];
-d10.sides = 10;
-
-var d12 = Object.create(dice);
-d12.history = [];
-d12.sides = 12;
-
-var d20 = Object.create(dice);
-d20.history = [];
-d20.sides = 20;
-
-var d100 = Object.create(dice);
-d100.history = [];
-d100.sides = 100;
+function moreDice() {
+    var sections = document.getElementsByTagName('section');
+    for (var i = 0; i < sections.length; i++) {
+        if (sections[i].style.display == 'none') {
+            sections[i].style.display = 'block';
+        } else {
+            sections[i].style.display = 'none';
+        }
+    }
+}
